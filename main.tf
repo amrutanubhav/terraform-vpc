06fcbce3adffa60707ddd70857cbe1b784867e29
Amrut Anubhav <anubhavamrut@gmail.com>
2023-03-28 16:33:10 +0530

    updating

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "./vendor/modules/vpc" ## source is now changed to this as terrafile is used to pull git repo to local
   VPC_CIDR = var.VPC_CIDR ## calling the variable within module
-  ENV      = var.ENV
+  ENV= var.ENV
 }
 
  
