84e912e44a8991bd4368385d82af761d10c715bf
Amrut Anubhav <anubhavamrut@gmail.com>
2023-04-01 12:47:44 +0530

    updating

--- output.tf
+++ output.tf
@@ -7,19 +7,19 @@ output "VPC_CIDR" {
 }
 
 output "PRIVATE_SUBNET_IDS" {
-    value = module.subnet.PRIVATE_SUBNET_IDS
+    value = module.vpc.PRIVATE_SUBNET_IDS
 }
 
 output "PUBLIC_SUBNET_IDS" {
-    value = module.subnet.PUBLIC_SUBNET_IDS
+    value = module.vpc.PUBLIC_SUBNET_IDS
 }
 
 output "PRIVATE_SUBNET_CIDR" {
-    value = module.subnet.PRIVATE_SUBNET_CIDR
+    value = module.vpc.PRIVATE_SUBNET_CIDR
 }
 
 output "PUBLIC_SUBNET_CIDR" {
-    value = module.subnet.PUBLIC_SUBNET_CIDR
+    value = module.vpc.PUBLIC_SUBNET_CIDR
 }
 
 output "DEFAULT_VPC_CIDR" {
